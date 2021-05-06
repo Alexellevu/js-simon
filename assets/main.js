6342c61868a5437127e3cdf4838bf7c8c0a6de95
alexellevu <newnapster83@gmail.com>
2021-05-06 17:19:16 +0200

    promt dei numeri da indovinare

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 
 
-//Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
-//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+
 
 //Un alert() espone 5 numeri generati casualmente.
 
@@ -13,15 +13,34 @@ function getrandom(min, max){
 var numeriRandom = [];
 for ( var i = 0; i < 5; i++){
 numeriRandom.push(getrandom(1, 6));
-alert(numeriRandom[i]);
-console.log(numeriRandom[i]);
 }
+console.log(numeriRandom);
+alert(numeriRandom);
+
 
 
 //Da li parte un timer di 30 secondi.
-/* setTimeout(tempoiniziale, 2000);
-    function tempoiniziale(){
+ setTimeout(ricordaNumeri, 3000);
+ //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+        function ricordaNumeri(){
+            var numeriUtente = [];
+            for( var j = 0; j < 5; j++ )
+            {
+            numeriUtente.push(prompt('inserisci uno per volta i numeri visualizzati in precedenza'));
+            var counter =0;
+            var numIndovinati = [];
+            if(numeriUtente.includes(numeriRandom)){
+                counter++;
+                numIndovinati.push(numeriUtente[j]);
+            }
+                
+        }
+            console.log(numIndovinati);
+            console.log(counter);
+            console.log(numeriUtente);
+            return numeriUtente;
+    }  
         
-    }
- */
 
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+        
