94ba2af3e405077f29175f94abbbe9eb1acc053e
alexellevu <newnapster83@gmail.com>
2021-05-06 16:02:37 +0200

    generazione dei 5 numeri casuali in un array e poi visualizzati nell'alert

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+
+//Da li parte un timer di 30 secondi.
+//Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
+//Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
+
+//Un alert() espone 5 numeri generati casualmente.
+
+function getrandom(min, max){
+  
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+
+var numeriRandom = [];
+for ( var i = 0; i < 5; i++){
+numeriRandom.push(getrandom(1, 6));
+}
+console.log(numeriRandom);
+alert(numeriRandom);
+
+
