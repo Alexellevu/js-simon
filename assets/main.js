7eb6d5be8ff4dfe60579c13b059dde195ec3c5f8
alexellevu <newnapster83@gmail.com>
2021-05-07 16:25:36 +0200

    correzioni if

--- assets/main.js
+++ assets/main.js
@@ -10,7 +10,7 @@ for ( var i = 0; i < 5; i++){
 numeriRandom.push(getrandom(1, 6));
 }
 console.log(numeriRandom);
-alert(numeriRandom);
+alert('memorizza i seguenti numeri ' + numeriRandom);
 
 
 
@@ -19,23 +19,22 @@ alert(numeriRandom);
  //Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
         function ricordaNumeri(){
             var numeriUtente = [];
-            for( var j = 0; j < 5; j++ )
-            {
+            for( var j = 0; j < 5; j++ ){
             numeriUtente.push(prompt('inserisci uno per volta i numeri visualizzati in precedenza'));
-            var counter =0;
+            
             var numIndovinati = [];
-            if(numeriUtente.includes(numeriRandom[j])){
-                counter++;
+            if(numeriRandom.includes(numeriUtente[j])){
+              
                 numIndovinati.push(numeriUtente[j]);
             }
                 
         }
             console.log(numIndovinati);
-            console.log(counter);
+            console.log(numIndovinati.length);
             console.log(numeriUtente);
-            return numeriUtente;
+            return numIndovinati;
     }  
         
-
+alert('hai indovinato ' + numIndovinati.length + ' numeri, ovvero:  ' + numIndovinati);
 //Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
         
